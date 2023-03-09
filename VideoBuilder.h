198228f4af8619b340356bd69821d12c422ce804
Matthew January <matthewjanuary@gmail.com>
2023-03-09 14:14:29 +0200

    Initialized git repo and began constructing header file. Constructors and Destructors in place

---
+++ VideoBuilder.h
@@ -0,0 +1,35 @@
+/*
+    Matthew January JNRMAT002
+    Header file for video builder assignment (Assignment 2)
+*/
+#ifndef MY_VIDEO_BLDR_H
+#define MY_VIDEO_BLDR_H
+
+// Includes
+#include <vector>
+#include <string>
+
+namespace JNRMAT002 {
+    class FrameSequence {
+    public:        
+        std::vector<unsigned char **> imageSequence;
+        int X1, Y1, X2, Y2, width, height;
+        std::string operation, fileName;
+
+        // Default Constructor for FrameSequence
+        FrameSequence(void);
+
+        // Constructor for FrameSequence
+        FrameSequence(int X1, int X2, int Y1, int Y2, int width, int height);
+
+        // Destructor for FrameSequence
+        ~FrameSequence();
+
+
+        
+    private:
+        
+    };
+}
+
+#endif
